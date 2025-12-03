cf01ad2490ce8c235d1b1be8048c2049bb851442
seeyafuture <jnvbhind1234@gmail.com>
2025-12-03 14:39:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const affirmations = [
-    "I am safe, I am strong, I am healing.",
-    "The universe is guiding me gently.",
-    "I release what doesn’t belong to me.",
-    "I am becoming the best version of myself.",
-    "My energy is calm, grounded and peaceful."
-];
-
-function newAffirmation() {
-    const box = document.getElementById("affirmText");
-    const random = affirmations[Math.floor(Math.random() * affirmations.length)];
-    box.innerText = random;
-}
+// script
